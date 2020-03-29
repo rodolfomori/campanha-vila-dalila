de243e7f9e39b7eb983bd3e2ba69d75ec2b0a77b
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-03-29 16:34:12 -0300

    automatic deploy

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://go5ousnc8i.execute-api.us-east-1.amazonaws.com/Production',
-  baseURL: 'http://localhost:3001',
-  // baseURL: 'https://api.viladalila.online',
+  // baseURL: 'http://localhost:3001',
+  baseURL: 'https://api.viladalila.online',
 });
 
 export default api;
