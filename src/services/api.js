57881017ad8065074a29063e276e4391053bf4d6
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-04-04 15:55:05 -0300

    add searchPhone

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
-  // baseURL: 'https://api.viladalila.online',
+  // baseURL: 'http://localhost:3001',
+  baseURL: 'https://api.viladalila.online',
 });
 
 export default api;
